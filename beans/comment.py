fe664cbba1dddcfac09b5a74062d6ef64c0ea206
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2021-05-09 17:18:53 +0200

    Add comment

--- beans/comment.py
+++ beans/comment.py
@@ -1,3 +1,11 @@
+"""
+All arguments are optional
+user_id: An unique identifier to tell users appart
+evidence_path: Path pointing to an image to be used as evidence
+user_name: Name of the user
+text_content: Text to display
+score: Positiveness of a comentary (negative below zero, positive avobe zero)
+"""
 class Comment:
     def __init__(self,
     user_id: str = None,
