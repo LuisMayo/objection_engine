5f7d8e092eb68af3ad5fee19aa14be499d6f1a08
Malcolm Anderson <malcolminyo@gmail.com>
2022-10-25 20:54:06 -0700

    Now parsing text into either character-by-character or chunk-by-chunk

--- objection_engine/parse_tags.py
+++ objection_engine/parse_tags.py
@@ -60,28 +60,103 @@ def add_line_breaks(text: str, tags: list[dict], max_chars: int = 15):
     while line_end_index < len(text) - 1:
         while line_end_index - line_start_index < max_chars:
             line_end_index += 1
-            print(f"Current line: \"{text[line_start_index:line_end_index]}\"")
 
         # If we're currently in the middle of a word, then move the line end index back to the previous space
         while line_end_index < len(text) and not text[line_end_index].isspace():
             line_end_index -= 1
 
         new_line = text[line_start_index:line_end_index]
-        new_line_length = len(new_line)
-        
-        # TODO: Strip whitespace from start and end
-        # TODO: Adjust tag indices to match:
-        # Difference in starting whitespace should be subtracted from all indices within this block and after?
-        # Difference in ending whitespace should be subtracted from all indices after this block?
-        # Not sure
+
+        new_line_tags = []
 
         
+        for tag in tags:
+            name = tag["name"]
+            start = tag["start"]
+            rel_start = start - line_start_index
+            local_start = max(0, rel_start)
+
+            new_tag = {
+                "name": name,
+                "start": local_start
+            }
+
+            if "end" in tag:
+                end = tag["end"]
+                rel_end = rel_start + (end - start)
+                local_end = min(len(new_line), rel_end)
+
+                # If this tag doesn't overlap at all with this line of text, then skip it
+                if rel_end < 0 or rel_start >= len(new_line):
+                    continue
+
+                new_tag["end"] = local_end
+                new_line_tags.append(new_tag)
+
+            else:
+                if rel_start < 0 or rel_start >= len(new_line):
+                    continue
+
+                new_line_tags.append(new_tag)
+
+        # Let's strip whitespace.
+        # First, strip whitespace from the beginning.
+        line_len_before_lstrip = len(new_line)
+        new_line = new_line.lstrip()
+        chars_removed = line_len_before_lstrip - len(new_line)
+        for tag in new_line_tags:
+            tag["start"] = max(0, tag["start"] - chars_removed)
+            if "end" in tag:
+                tag["end"] = max(0, tag["end"] - chars_removed)
+
+        # Next, strip whitespace from the end.
+        line_len_before_rstrip = len(new_line)
+        new_line = new_line.rstrip()
+        chars_removed = line_len_before_rstrip - len(new_line)
+        for tag in new_line_tags:
+            if "end" in tag:
+                tag["end"] = min(tag["end"] - chars_removed, len(new_line))
+
         # Go to next line
-        lines.append(text[line_start_index:line_end_index])
+        lines.append((new_line, new_line_tags))
         line_start_index = line_end_index
-    
-    print(lines)
+
+    return lines
+
+def line_to_character_chunks(text: str, tags: list[dict]):
+    return [
+        (c, [
+            item["name"] for item in tags
+            if "end" not in item and item["start"] == i
+            or "end" in item and i in range(item["start"], item["end"])
+            ])
+        for i, c in enumerate(text)
+    ]
+
+def merge_character_chunks(characters: list[tuple[str, list[dict]]]):
+    current_string = ""
+    current_tags = characters[0][1]
+    chunks = []
+    for c, tags in characters:
+        c: str
+        tags: list[dict]
+        if tags == current_tags:
+            current_string += c
+        else:
+            chunks.append((current_string, current_tags))
+            current_string = ""
+            current_tags = tags.copy()
+            current_string += c
+
+    chunks.append((current_string, current_tags))
+    return chunks
 
 test_string = "Hello, my name is <red>Bob</red> and I like to <green>eat avocados</green>. <shake/>Yum! <blue>(Wish I had more...)</blue>"
+
 text, tags = parse_text(test_string)
-add_line_breaks(text, tags, 20)
+lines = add_line_breaks(text, tags, 40)
+character_chunk_lines = [line_to_character_chunks(*i) for i in lines]
+continuous_chunks = [merge_character_chunks(line) for line in character_chunk_lines]
+print(character_chunk_lines)
+print()
+print(continuous_chunks)
