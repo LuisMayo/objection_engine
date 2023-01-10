38ce76b076d9697643dac11c840e6bb770066d34
Malcolm Anderson <malcolminyo@gmail.com>
2023-01-09 21:55:06 -0800

    Improved example/profiling test script

--- examples/v4/example_compare_v3_and_v4.py
+++ examples/v4/example_compare_v3_and_v4.py
@@ -3,7 +3,7 @@ Test script to compare performance of the v3 engine and v4 engine
 on identical comment chains.
 """
 from timeit import default_timer as timer
-from time import time
+from time import strftime
 from objection_engine.beans.comment import Comment
 from rich import print
 import cProfile
@@ -48,34 +48,39 @@ comments = [
     ),
 ]
 
-current_time = int(time())
+current_time = strftime("%Y-%m-%d_%H-%M-%S")
 
-# # Test importing and rendering with v3 engine
-before_v3 = timer()
-from objection_engine.renderer import render_comment_list as render_comment_list_v3
-render_comment_list_v3(comment_list=comments, output_filename=f"output_3vs4_v3-{current_time}.mp4")
-after_v3 = timer()
+def v3_test():
+    before_v3 = timer()
+    from objection_engine.renderer import render_comment_list as render_comment_list_v3
+    render_comment_list_v3(comment_list=comments, output_filename=f"output_3vs4_v3-{current_time}.mp4")
+    after_v3 = timer()
+    print("Time for v3:", f"{after_v3 - before_v3:.2f} s")
 
-print(f"v3 done, took {after_v3 - before_v3:.2f} s")
 
-# Do initialization stuff ahead of time (especially loading the sentiment
-# analysis model)
-from objection_engine.v4.ace_attorney_scene import DialogueBoxBuilder
-builder = DialogueBoxBuilder()
+def v4_test():
+    before_v4 = timer()
+    from objection_engine.v4.ace_attorney_scene import DialogueBoxBuilder
+    builder = DialogueBoxBuilder()
+    builder.render(
+        comments=comments,
+        output_filename=f"output_3vs4_v4-{current_time}.mp4",
+    )
+    after_v4 = timer()
+    print("Time for v4:", f"{after_v4 - before_v4:.2f} s")
+
+def test_both():
+    v3_test()
+    v4_test()
 
 # Test importing and rendering with v4 engine
 pr = cProfile.Profile()
-before_v4 = timer()
 pr.enable()
-builder.render(
-    comments=comments,
-    output_filename=f"output_3vs4_v4-{current_time}.mp4",
-)
+test_both()
 pr.disable()
-after_v4 = timer()
-pr.dump_stats(f"output_3vs4_4profile-{current_time}")
+pr.dump_stats(f"output_3vs4_4profile-{current_time}.prof")
 
 
 
-print("Time for v3:", f"{after_v3 - before_v3:.2f} s")
-print("Time for v4:", f"{after_v4 - before_v4:.2f} s")
+# 
+
