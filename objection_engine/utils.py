c1ed8e37708903db9394614d0c90fee89bb0eb61
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2023-03-22 12:51:27 +0100

    Added migration script to new assets

--- objection_engine/utils.py
+++ objection_engine/utils.py
@@ -4,30 +4,54 @@ from collections import Counter
 import os
 import requests
 import zipfile
-
+import shutil
+import json
+from toml import dump
 
 def ensure_assets_are_available():
     if not os.path.exists('assets') or not os.listdir('assets'):
-        print('Assets not present. Downloading them')
-        response = requests.get('https://dl.luismayo.com/assets.zip')
-        with open('assets.zip', 'wb') as file:
-            file.write(response.content)
-        with zipfile.ZipFile('assets.zip', 'r') as zip_ref:
-            zip_ref.extractall('assets')
+        download_assets()
         os.remove('assets.zip')
     else:
         # This is in case there are only some missing assets that have been added in updates
-        updated_assets = []
-        updated_assets.append('arrow.gif')
-        updated_assets.append('igiari/KawkabMono-Regular.ttf')
-        for asset in updated_assets:
-            full_path = './assets/' + asset
-            dl_path = 'https://dl.luismayo.com/objection_engine/' + asset
-            if not os.path.isfile(full_path):
-                print(f'Assets present. but {asset} is missing, downloading')
-                response = requests.get(dl_path)
-                with open(full_path, 'wb') as file:
-                    file.write(response.content)
+        detect_old_assets_format()
+
+def download_assets():
+    print('Assets not present. Downloading them')
+    response = requests.get('https://dl.luismayo.com/assetsv4.zip')
+    with open('assets.zip', 'wb') as file:
+        file.write(response.content)
+    with zipfile.ZipFile('assets.zip', 'r') as zip_ref:
+        zip_ref.extractall('assets')
+
+def detect_old_assets_format():
+    if os.path.exists('./assets/igiari'):
+        print("Old assets format detected. Moving assets folder to assets_old")
+        os.rename("./assets", "./assets_old")
+        download_assets()
+        print("Migrating music (if any) to the new assets folder with the new assets config")
+        for music_folder in os.listdir("./assets_old/music"):
+            if not os.path.exists("./assets/music/" + music_folder):
+                print("Migrating " + music_folder)
+                try:
+                    shutil.copytree("./assets_old/music/" + music_folder, "./assets/music/" + music_folder)
+                except Exception as e:
+                    print("Error while copying" + str(e))
+                    continue 
+                try:
+                    with open("./assets/music/" + music_folder + "/config.json",'rt') as file_json:
+                        old_config = json.load(file_json)
+                        with open("./assets/music/" + music_folder + "/config.toml",'wt') as file_toml:
+                            dump(old_config, file_toml)
+                except Exception as e:
+                    print("Error trying to convert the music format config file. Removing the folder")
+                    try:
+                        shutil.rmtree("./assets/music/" + music_folder)
+                    except Exception as e2:
+                        print("Error while trying to remove the folder. Music may be corrupted")
+            else:
+                print("Folder " + music_folder + " already existed on destination, omiting migration")
+
 
 
 def get_characters(common: Counter, assigned_characters: dict = None, adult_mode: bool = False):
