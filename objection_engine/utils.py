9021ed926daacbe336318497c7cc72dd325fa5b5
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2023-03-23 17:15:50 +0100

    Redirecting get music to the new method

--- objection_engine/utils.py
+++ objection_engine/utils.py
@@ -1,5 +1,4 @@
-from .constants import Character
-import random
+from .loading import load_music_data 
 from collections import Counter
 import os
 import requests
@@ -25,7 +24,7 @@ def download_assets():
         zip_ref.extractall('assets')
 
 def detect_old_assets_format():
-    if os.path.exists('./assets/igiari'):
+    if os.path.exists('./Sprites-phoenix'):
         print("Old assets format detected. Moving assets folder to assets_old")
         os.rename("./assets", "./assets_old")
         download_assets()
@@ -52,85 +51,17 @@ def detect_old_assets_format():
             else:
                 print("Folder " + music_folder + " already existed on destination, omiting migration")
 
-
-
-def get_characters(common: Counter, assigned_characters: dict = None, adult_mode: bool = False):
-    users_to_characters = {} if assigned_characters is None else assigned_characters.copy()
-    most_common =  [t[0] for t in common.most_common()]
-    all_rnd_characters = [
-        Character.GODOT,
-        Character.FRANZISKA,
-        Character.JUDGE,
-        Character.LARRY,
-        Character.MAYA,
-        Character.KARMA,
-        Character.PAYNE,
-        Character.MAGGEY,
-        Character.LOTTA,
-        Character.GUMSHOE,
-        Character.GROSSBERG,
-        Character.APOLLO,
-        Character.KLAVIER,
-        Character.MIA,
-        Character.WILL,
-        Character.OLDBAG,
-        Character.REDD,
-    ]
-
-    if not adult_mode:
-        all_rnd_characters.append(Character.PEARL)
-
-    # Confirm that all of the assigned characters are valid.
-    all_characters = all_rnd_characters + [Character.PHOENIX, Character.EDGEWORTH]
-    for character in users_to_characters.values():
-        if character not in all_characters:
-            raise ValueError(f"\"{character}\" is not a valid character")
-
-    # If Phoenix was not manually assigned and the user with
-    # the most comments wasn't manually assigned a character,
-    # assign Phoenix to the user with the most comments.
-    try:
-        if Character.PHOENIX not in users_to_characters.values() and most_common[0] not in users_to_characters:
-            users_to_characters[most_common[0]] = Character.PHOENIX
-    except IndexError:
-        pass
-
-    # Same for Edgeworth, but to the user with the second-most comments.
-    try:
-        if Character.EDGEWORTH not in users_to_characters.values() and most_common[1] not in users_to_characters:
-            users_to_characters[most_common[1]] = Character.EDGEWORTH
-    except IndexError:
-        pass
-
-    # Finally, from the remaining pool of characters, let's assign them randomly
-    # to the remaining user IDs.
-    rnd_characters = [c for c in all_rnd_characters if c not in users_to_characters.values()]
-    for user_id in most_common:
-        # Skip users who were manually assigned characters.
-        if user_id in users_to_characters:
-            continue
-
-        # Reload the choosable characters, if we ran out.
-        if len(rnd_characters) == 0:
-            rnd_characters = all_rnd_characters.copy()
-
-        # Assign a character randomly chosen from the list to this user,
-        # and remove them from the pool.
-        rnd_character = random.choice(rnd_characters)
-        rnd_characters.remove(rnd_character)
-        users_to_characters[user_id] = rnd_character
-
-    return users_to_characters
-
 def get_all_music_available():
     ensure_assets_are_available()
-    available_music = os.listdir('assets/music')
-    available_music.append('rnd')
-    return available_music
+    available_music = load_music_data()
+    list = []
+    for key in available_music.keys():
+        list.append(key)
+    list.append('rnd')
+    return list
 
 def is_music_available(music: str) -> bool:
     music = music.lower()
-    ensure_assets_are_available()
-    available_music = os.listdir('assets/music')
+    available_music = get_all_music_available()
     available_music.append('rnd')
     return music in available_music
