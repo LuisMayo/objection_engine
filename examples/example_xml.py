0dff80cce04261320d17ec0f0068c71b22129821
Malcolm Anderson <malcolminyo@gmail.com>
2023-08-08 20:37:37 -0700

    XML parser (WIP)

---
+++ examples/example_xml.py
@@ -0,0 +1,12 @@
+from objection_engine.xml_parser import parse_script
+from objection_engine.ace_attorney_scene import AceAttorneyDirector
+from os.path import dirname, realpath, join
+from time import time
+from rich import print
+
+pages = parse_script(join(dirname(realpath(__file__)), "test_script.xml"))
+
+print(pages)
+director = AceAttorneyDirector()
+director.set_current_pages(pages)
+director.render_movie(output_filename=f'example-xml-{int(time())}.mp4')
